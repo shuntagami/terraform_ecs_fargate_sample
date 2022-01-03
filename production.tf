97d18dbbb7883717c8aef9708fe026609289e78a
shuntagami <shun.tagami@i-nations.tokyo>
2022-01-03 13:49:01 +0900

    Implement RDS of which engine is postgres v13.3

--- production.tf
+++ production.tf
@@ -24,3 +24,18 @@ module "networking" {
   availability_zones   = local.production_availability_zones
   key_name             = aws_key_pair.key_pair.key_name
 }
+
+module "rds" {
+  depends_on        = [module.networking]
+  source            = "./modules/rds"
+  environment       = "production"
+  allocated_storage = "20"
+  engine            = "postgres"
+  engine_version    = "13.3"
+  database_name     = var.production_database_name
+  database_username = var.production_database_username
+  database_password = var.production_database_password
+  subnet_ids        = "${module.networking.private_subnets_id}"
+  vpc_id            = module.networking.vpc_id
+  instance_class    = "db.t3.micro"
+}
